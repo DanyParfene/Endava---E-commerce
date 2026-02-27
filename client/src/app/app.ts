dcaedfa5149c25174c3577e1fde9e59973e8d625
SchiporDevis <devis.schipor@outlook.com>
2026-02-27 16:45:04 +0200

    Sidebar
    Created sidebar and sidebar category + responsive design

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { RedirectButton } from './components/redirect-button/redirect-button';
 import { ProductCard } from './components/product-card/product-card';
+import { Sidebar } from './components/sidebar/sidebar';
+import { SidebarCategory } from './components/sidebar-category/sidebar-category';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, RedirectButton, ProductCard],
+  imports: [RouterOutlet, Header, RedirectButton, ProductCard, Sidebar, SidebarCategory],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
